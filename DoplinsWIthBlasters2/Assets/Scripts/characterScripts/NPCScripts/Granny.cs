15ea61dee1a882fa878ea7c0b43f43bb8ebe4e37
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-09-06 16:40:11 +0200

    particles

--- DoplinsWIthBlasters2/Assets/Scripts/characterScripts/NPCScripts/Granny.cs
+++ DoplinsWIthBlasters2/Assets/Scripts/characterScripts/NPCScripts/Granny.cs
@@ -10,49 +10,56 @@ public class Granny : MonoBehaviour {
 	[SerializeField]
 	private int[] woodNeeded = new int[8];
 	private int _upgradeLevel = 0;
-    public GameObject textbox;
-    private bool playerIsNearby;
-    public Player player;
+	public GameObject textbox;
+	private bool playerIsNearby;
+	public Player player;
 	public GameObject[] upgrade = new GameObject[8];
 	[SerializeField]
 	private Inventory _playerinventory;
 
-    private void Update()
-    {
+	Text text;
+
+	void Start()
+	{
+		text = textbox.GetComponentInChildren<Text> ();
+	}
+
+	private void Update()
+	{
 		if (Input.GetKeyDown(KeyCode.E) && playerIsNearby && _playerinventory.CoinAmount >= goldNeeded[_upgradeLevel])
-        {
+		{
 			GameEventManager.UpgradeCost = goldNeeded[_upgradeLevel];
-            //player.RemoveGold(goldNeeded);
-            UpgradeTown();
-        }
-    }
+			//player.RemoveGold(goldNeeded);
+			UpgradeTown();
+		}
+	}
 
-    private void UpgradeTown()
-    {
+	private void UpgradeTown()
+	{
 		upgrade[_upgradeLevel].SetActive(true);
+		text.text = "Come back with " + goldNeeded[_upgradeLevel] + " gold.";
 		_upgradeLevel++;
-    }
+	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            Text text = textbox.GetComponentInChildren<Text>();
-            text.text = "Come back with " + goldNeeded + " gold.";
-            Vector3 pos = transform.position;
-            pos.y = 1;
-            textbox.transform.position = pos;
-            textbox.SetActive(true);
-            playerIsNearby = true;
-        }
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			text.text = "Come back with " + goldNeeded[_upgradeLevel] + " gold.";
+			Vector3 pos = transform.position;
+			pos.y =2;
+			textbox.transform.position = pos;
+			textbox.SetActive(true);
+			playerIsNearby = true;
+		}
+	}
 
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.tag == "Player")
-        {
-            playerIsNearby = false;
-            textbox.SetActive(false);
-        }
-    }
+	private void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			playerIsNearby = false;
+			textbox.SetActive(false);
+		}
+	}
 }
