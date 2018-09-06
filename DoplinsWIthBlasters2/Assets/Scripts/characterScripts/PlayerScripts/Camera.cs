2af18c00908eca62bf6c6d041891ff21479b09dd
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-09-06 11:46:05 +0200

    vertical slice

--- DoplinsWIthBlasters2/Assets/Scripts/characterScripts/PlayerScripts/Camera.cs
+++ DoplinsWIthBlasters2/Assets/Scripts/characterScripts/PlayerScripts/Camera.cs
@@ -7,7 +7,7 @@ public class Camera : MonoBehaviour {
     public GameObject target;
 	
 	void Update () {
-        Vector3 pos = new Vector3(target.transform.position.x, 7, target.transform.position.z - 2);
+        Vector3 pos = new Vector3(target.transform.position.x, 10, target.transform.position.z - 8);
         transform.position = pos;
 	}
 }
