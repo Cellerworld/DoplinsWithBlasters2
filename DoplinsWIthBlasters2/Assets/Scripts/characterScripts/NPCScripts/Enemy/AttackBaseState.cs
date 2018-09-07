2b944fb9f2231fd2485ea0978f7d9cee97228416
DESKTOP-P1DU2EF\kraem <kraemerfa@t-online.de>
2018-09-07 15:05:02 +0200

    AI statemachine
    need some updates within its start method

---
+++ DoplinsWIthBlasters2/Assets/Scripts/characterScripts/NPCScripts/Enemy/AttackBaseState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBaseState : AbstractState
+{
+    private static AttackBaseState _instance;
+
+    public static AttackBaseState GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new AttackBaseState();
+        }
+        return _instance;
+    }
+
+    public override void Enter(EnemyAgent agent)
+    {
+        agent.GetNavMeshAgent().isStopped = true;
+        agent.ResetAttack();
+        //start attack animation
+        agent.Attack();
+    }
+
+    public override void Update(EnemyAgent agent)
+    {
+        
+        if (agent.GetCurrentTimeBtwAttacks() <= 0)
+        {
+            agent.Attack();
+        }
+        else
+        {
+            agent.AdjustAttackTime();
+        }
+    }
+
+    public override void Exit(EnemyAgent agent)
+    {
+    }
+}
---
+++ DoplinsWIthBlasters2/Assets/Scripts/characterScripts/NPCScripts/Enemy/AttackState.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class AttackState : AbstractState
+{
+    private static AttackState _instance;
+
+    public static AttackState GetInstance()
+    {
+        if(_instance == null)
+        {
+            _instance = new AttackState();
+        }
+        return _instance;
+    }
+
+    public override void Enter(EnemyAgent agent)
+    {
+        agent.ResetAttack();
+        //start attack animation
+        agent.Attack();
+    }
+
+    public override void Update(EnemyAgent agent)
+    {
+        //check if target still in attack range if yes attack again after ending the attack animation
+        agent.SetPlayerTarget();
+        //Debug.Log(Vector3.Distance(agent.GetNavMeshAgent().destination, agent.transform.position));
+        if(Vector3.Distance(agent.GetNavMeshAgent().destination, agent.transform.position) < agent.GetAttackRange())
+        {
+            agent.GetNavMeshAgent().isStopped = true;
+            if(agent.GetCurrentTimeBtwAttacks() <= 0)
+            {
+                agent.Attack();
+            }
+            else
+            {
+                agent.AdjustAttackTime();
+            }
+        }
+        else
+        {
+            agent.SetState(FollowPlayerState.GetInstance());
+        }
+    }
+
+    public override void Exit(EnemyAgent agent)
+    {
+        Debug.Log("Dont run away coward!");
+        agent.GetNavMeshAgent().isStopped = false;
+    }
+}
